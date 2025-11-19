7c2e56e1cb17170ce2dca3f962d3af93628e8be7
Adam Halka <galkaadam4@gmail.com>
2025-11-19 12:25:11 +0100

    Commit

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -144,5 +144,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
